b47d9286b6c95c354535920bb09f11b061d91a43
Vinicius Hirata <viniciusadamucci@hotmail.com>
2024-06-12 16:55:41 -0300

    arruma acentuação

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const port = 3000;
 
 const server = http.createServer((request, response) => {
+    response.setHeader('content-type','text/html; charset=utf8')
     response.write('Olá HTTP!');
     response.end();
 });
@@ -10,3 +11,4 @@ const server = http.createServer((request, response) => {
 server.listen(3000, () => {
     console.log('servidor iniciado na porta', port);
 });
+
